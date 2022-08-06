c2a5fe97c28262edafd335d24f8af4a3661400a3
Babs1995 <106346978+Babs1995@users.noreply.github.com>
2022-08-05 21:59:19 -0700

    first comit

---
+++ client/src/App.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import {
+  BrowserRouter,
+  Route,
+  Switch
+} from 'react-router-dom';
+import Header from './components/Header';
+import Courses from './components/Courses.js';
+import CreateCourse from './components/CreateCourse';
+import UpdateCourse from './components/UpdateCourse';
+import CourseDetail from './components/CourseDetail.js';
+import UserSignIn from './components/UserSignIn.js';
+import UserSignUp from './components/UserSignUp';
+import UserSignOut from './components/UserSignOut';
+import NotFound from './components/NotFound';
+import Forbidden from './components/Forbidden';
+import UnhandledError from './components/UnhandledError';
+
+import './global.css';
+import {withContext} from './Context';
+import PrivateRoute from './PrivateRoute';
+
+const HeaderWithContext = withContext(Header);
+const UserSignInWithContext = withContext(UserSignIn);
+const UserSignUpWithContext = withContext(UserSignUp);
+const UserSignOutWithContext = withContext(UserSignOut);
+
+export default function App() {
+
+  return (
+    <BrowserRouter>
+      <HeaderWithContext />
+
+      <Switch>
+        <Route exact path='/' component={Courses} />
+        <Route exact path='/courses' component={Courses} />
+        <PrivateRoute path="/courses/create" component={CreateCourse} />
+        <PrivateRoute path ='/courses/:id/update' component={UpdateCourse} />
+        <Route exact path='/courses/:id' component={CourseDetail} />
+        <Route path='/signin' component={UserSignInWithContext} /> 
+        <Route path='/signup' component={UserSignUpWithContext} />
+        <Route path='/signout' component={UserSignOutWithContext} />
+        <Route path='/error' component={UnhandledError} />
+        <Route path='/forbidden' component={Forbidden} />
+        <Route component={NotFound} />
+        
+      </Switch>
+    </BrowserRouter>
+  );
+}
---
+++ client/src/components/Header.js
@@ -0,0 +1,39 @@
+
+
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default class Header extends React.PureComponent {
+  render() {
+    const { context } = this.props;
+    const authUser = context.authenticatedUser;
+
+    //** Renders the HTML **/
+    return (
+        <header>
+            <div className="wrap header--flex">
+                <h1 className="header--logo"><Link to="/courses">Courses</Link></h1>
+                <nav>
+                {/* If there is an authorized user, a Welcome span and Sign Out button appear. */}
+                {/* If there is not an authorized user, a Sign In and Sign Up button appear. */}
+                    {authUser ?
+                        <ul className='header--signedin'>
+                            <React.Fragment>
+                                <li><span>Welcome, {authUser.firstName} {authUser.lastName}!</span></li>
+                                <li><Link to="/signout">Sign Out</Link></li>
+                            </React.Fragment>
+                        </ul>
+                    : 
+                        <ul className='header--signedout'>
+                            <React.Fragment>
+                                <li><Link className="signup" to="/signup">Sign Up</Link></li>
+                                <li><Link className="signin" to="/signin">Sign In</Link></li>
+                            </React.Fragment>
+                        </ul>
+                    }
+                </nav>
+            </div>
+        </header>
+    );
+  }
+};
